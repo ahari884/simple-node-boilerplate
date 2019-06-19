4ad4dec0973ed58a883c8a0f13cb9bc62bc3d178
allakonda harish <ahari884@gmail.com>
2019-06-19 20:22:10 +0530

    Removed mongoose deprication warning

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,7 +5,7 @@ var glob = require('glob');
 
 module.exports = function () {
     mongoose.Promise = global.Promise;
-    db = mongoose.connect(config.dbUrl).then(function () {
+    db = mongoose.connect(config.dbUrl, { useNewUrlParser: true, useCreateIndex: true }).then(function () {
         console.log('Connected to database successfully', config.dbUrl);
     }, function (err) {
         console.log('Database connection timeout error');
